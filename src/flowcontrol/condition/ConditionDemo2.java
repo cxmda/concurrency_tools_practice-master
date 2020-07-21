0f65a928f30b42997ed3cc130e2fd61f7986f595
chenqiang <saumo136@gmail.com>
2020-07-21 21:36:08 +0800

    Condition

---
+++ src/flowcontrol/condition/ConditionDemo2.java
@@ -0,0 +1,89 @@
+package flowcontrol.condition;
+
+import java.util.PriorityQueue;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+/**
+ * 演示用Condition实现生产者消费者模式
+ *
+ * @author chen
+ * @create 2020-07-20 22:10
+ */
+public class ConditionDemo2 {
+
+    private int queueSize = 10;
+    private PriorityQueue<Integer> queue = new PriorityQueue<>(queueSize);
+    private Lock lock = new ReentrantLock();
+    private Condition notFull = lock.newCondition();
+    private Condition notEmpty = lock.newCondition();
+
+    public static void main(String[] args) {
+        ConditionDemo2 conditionDemo2 = new ConditionDemo2();
+        Consumer consumer = conditionDemo2.new Consumer();
+        Producer producer = conditionDemo2.new Producer();
+        consumer.start();
+        producer.start();
+    }
+
+    class Consumer extends Thread {
+
+        @Override
+        public void run() {
+            consume();
+        }
+
+        private void consume() {
+            while (true) {
+                lock.lock();
+                try {
+                    while (queue.size() == 0) {
+                        System.out.println("队列空，等待数据");
+                        try {
+                            notEmpty.await();
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                    queue.poll();
+                    notFull.signal();
+                    System.out.println("从队列中取出一个数据，还剩" + queue.size() + "个元素");
+                } finally {
+                    lock.unlock();
+                }
+            }
+        }
+    }
+
+    class Producer extends Thread {
+
+        @Override
+        public void run() {
+            producer();
+        }
+
+        private void producer() {
+            while (true) {
+                lock.lock();
+                try {
+                    while (queue.size() == queueSize) {
+                        System.out.println("队列满，等待有空余");
+                        try {
+                            notFull.await();
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                    queue.offer(1);
+                    notEmpty.signal();
+                    System.out.println("向队列中插入一个数据，队列剩余空间" + (queueSize - queue.size()));
+                } finally {
+                    lock.unlock();
+                }
+            }
+        }
+    }
+}
+
+
